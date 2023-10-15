9a744a907a032ce5a8e2eaaf3e91650f56ed7300
Hasan YASEN <ysnhasan1@gmail.com>
2023-10-15 18:35:06 +0300

    Update store.js

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -3,6 +3,6 @@ import counterSlice from "../features/counter/counterSlice";
 
 export const store = configureStore({
     reducer: {
-        counter: counterSlice // Diğer sayfalarda "counter" olarak kullanacağız.
+        counterReducer: counterSlice // Diğer sayfalarda "counterReducer" olarak kullanacağız.
     },
 })
